f46350ca17b5047fc6ab9851a6dcd871979fd4f4
Dovydas Stankevicius <ds@gangverk.is>
2018-11-30 20:13:32 +0000

    refactoring questions

--- src/javascript/hackme/assets/js/crunch.js
+++ src/javascript/hackme/assets/js/crunch.js
@@ -1,38 +1,33 @@
 $(document).ready(function () {
+	var introText = `[+] Insert information about the victim to make a dictionary \n[+] If you dont know all of the information, just hit ENTER when asked \n`;
+	var endText = `[+] Generating dictionary... \n[+] Sorting list and removing dublicate... \n[+] Saving dictionary to pass.txt, counting 4322 words.`;
+	var questions = {
+		0:"> First Name: ",
+		1:"> Surname: ",
+		2:"> Nickname: ",
+		3:"> Birthdate (DDMMYYYY): ",
+		4:"> Partners name: ",
+		5:"> Partner surname: ",
+		6:"> Partner\'s nickname: ",
+		7:"> Partner\'s birthdate (DDMMYYYY): ",
+		8:"> Pet\'s name: ",
+		9:"> Company name: ",
+		10:"> Street name: ",
+	};
 
 	$ptty.register('command', {
 		name: 'cr',
 		method : function(cmd){
-			cmd.out = '[+] Insert information about the victim to make a dictionary \n[+] If you dont know all of the information, just hit ENTER when asked \n';
+			cmd.out = introText;
 			cmd.next = 'cr 0';
 			cmd.ps = '(enter to continue)';
 			if(cmd[1]){
 				var num = parseInt(cmd[1], 10);
 				cmd.next = 'cr '+(num+1);
-				if(num == 0){
-					cmd.out = '> First Name: ';
-				}else if(num == 1){
-					cmd.out = '> Surname: ';
-				}else if(num == 2){
-					cmd.out = '> Nickname: ';
-				}else if(num == 3){
-					cmd.out = '> Birthdate (DDMMYYYY): ';
-				} else if(num == 4){
-					cmd.out = '> Partners name: ';
-				} else if(num == 5){
-					cmd.out = '> Partner\'s nickname: ';
-				} else if(num == 6){
-					cmd.out = '> Partner\'s birthdate (DDMMYYYY): ';
-				} else if(num == 7){
-					cmd.out = '> Partner\'s birthdate (DDMMYYYY): ';
-				} else if(num == 8){
-					cmd.out = '> Pet\'s name: ';
-				} else if(num == 8){
-					cmd.out = '> Company name: ';
-				} else if(num == 8){
-					cmd.out = '> Street name: ';
-				} else{
-					cmd.out = '[+] Generating dictionary... \n[+] Sorting list and removing dublicate... \n[+] Saving dictionary to pass.txt, counting 4322 words.';
+				if (num < Object.keys(questions).length){
+					cmd.out = questions[num];
+				} else {
+					cmd.out = endText;
 					cmd.ps = cmd.next = null; // end game.
 				}
 			}
