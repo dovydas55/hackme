d9153beb3dedb2b6d24f4df5d53af75e1ebfa349
Ingimar Svanberg Jóhannesson <ingimar.johannesson@gmail.com>
2018-11-30 21:06:18 +0000

    got hydra working

--- src/javascript/hackme/assets/js/hydra.js
+++ src/javascript/hackme/assets/js/hydra.js
@@ -7,23 +7,36 @@ function wait(ms){
 }
 
 var hydraFunc = function (cmd) {
-	let filename = cmd[1];
-	let passwords = ["afdog33", "cat89"];
-
-	if(!(filename in filesystem))
+	if(!(1 in cmd))
 	{
-		cmd.out = "cat: "+filename+": no such file";
+		cmd.out = "hydra: missing file operand";
 		return cmd;
 	}
 
-	$ptty.echo("Hi")
+	let filename = cmd[1];
 
-	for (var i = passwords.length - 1; i >= 0; i--) {
-		$ptty.echo("[ATTEMPT] - pass \""+passwords[i]+"\"");
-		wait(1000);
+	if(!(filename in filesystem))
+	{
+		cmd.out = "hydra: "+filename+": no such file";
+		return cmd;
 	}
 
-	return cmd;
+	//let passwords = ["afdog33", "cat89", "ble", "haha","afdog33", "cat89", "ble", "haha","afdog33", "cat89", "ble", "haha","afdog33", "cat89", "ble", "haha"];
+	let passwords = filesystem[filename].split("\n");
+
+	for (var i = passwords.length - 1; i >= 0; i--) {
+		let pass =  passwords[i];
+		let length = passwords.length;
+		let index = i+1;
+		window.setTimeout(function(){
+            $ptty.echo(`[ATTEMPT] - pass \"${pass}\" ${index}/${length}`);
+        }, 100 + 150 * i);
+	}
+
+	cmd.out = "Hydra v8.2 (c) 2016 - Please do not use in military or secret service opganizations, or for illegal purposes.\n\nHydra starting at (2018-11-30 20:17:05)\n";
+
+	$ptty.set_command_option(cmd);
+	return false;
 }
 
 
@@ -32,7 +45,7 @@ $(document).ready(function () {
 	$ptty.register('command', {
 		name: 'hydra',
 		method: hydraFunc,
-		options: [1],
+		options: [1, "-i"],
 		help: 'Will add file to the directory'
 	});
 });
