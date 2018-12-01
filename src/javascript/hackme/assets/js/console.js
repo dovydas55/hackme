a0328a7169d8a52c6c5505c857986a128b29d7d9
Ingimar Svanberg Jóhannesson <ingimar.johannesson@gmail.com>
2018-12-01 01:27:47 +0000

    updated story

--- src/javascript/hackme/assets/js/console.js
+++ src/javascript/hackme/assets/js/console.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 			welcome: `Kali linux${" "+(new Date()).toISOString().slice(0,19)}<br>Type <b>help</b> to list the available commands.`
 		}
 	});
-	$ptty.echo("Hello noob, listen Up!\nOur g3n3r4tor hacker went on a vacation for a month to Thailand.\nWe need you to hack this guys account, while our other hacker is away.\nWe need to to get into his account and write into his status this text:\nThe president is a lier. Mister Trump should know that I have never even meet his wife.\nAnd to accuse me of such illfallen deeds. I have never had sex with THAT woman!\nHe is a bigget and a traitor!<br><br>")
+	$ptty.echo("Hello noob, listen Up!\nOur g3n3r4tor hacker went on a vacation for a month to Thailand.\nWe need you to hack this guys account, while our other hacker is away.\nWe need to to get into his account and write into his status this text:\n\tThe president is a lier. Mister Trump should know\n\tthat I have never even meet his wife.\n\tAnd to accuse me of such illfallen deeds.\n\tI have never had sex with THAT woman!\n\tHe is a bigget and a traitor!\nYou can find his email and other data on his profile.\nYou need to harvest the data and put it into the Rainbow table generator command \"cr\"\nIf you dont know how it works then just write help.\n\n")
 	$ptty.register('command', {
 		name: 'ls',
 		method: function (cmd) {
