69e5cc3a51b4aa19d5820b65c05b32ff3ccf32e7
Ingimar Svanberg Jóhannesson <ingimar.johannesson@gmail.com>
2019-04-27 03:47:26 +0000

    updated game to use canvas

--- ld-44/client/game.js
+++ ld-44/client/game.js
@@ -13,50 +13,78 @@ websocket.onerror = (err) => {
 	console.log(err);
 }
 
+/*
+{
+    type: "move",
+    move: "left"
+}
+
+{
+    type: "new",
+    player_id: 1,
+    pos: [1,1],
+    color: "green"
+}
+*/
+
+let players = [];
+
 websocket.onmessage = (msg) => {
 	response = JSON.parse(msg.data)
 	switch (response.type) {
 		case 'USER_JOIN_EVENT':
-			console.log(response)
+            players = [];
+            for(i = 0; i < response.event.length; ++i)
+            {
+                createPlayer(response.event[i].user_id);
+            }
 			break;
 		case 'USER_MOVE_EVENT':
-			switch (response.event.direction) {
-				case 'left':
-					leftArrowPressed();
-					console.log(response)
-					break;
-				case 'right':
-					rightArrowPressed();
-					break;
-				case 'up':
-					upArrowPressed();
-					break;
-				case 'down':
-					downArrowPressed();
-					break;
-			}
+            console.log(response)
+            move(response.event.user_id, response.event.direction);
+            break;
+	}
+}
+
+function move(playerId, movement)
+{
+    var player = players.find(function(i){
+        return i.id == playerId;
+    })
+    console.log(player)
+    if(!player) return;
+    switch (movement)
+    {
+		case 'left':
+            player.x -= 5;
+			break;
+		case 'right':
+            player.x += 5;
+        	break;
+		case 'up':
+            player.y -= 5;
+			break;
+		case 'down':
+            player.y += 5;
 			break;
 	}
 }
 
-function leftArrowPressed() {
-	var element = document.getElementById("image1");
-	element.style.left = parseInt(element.style.left) - 5 + 'px';
+function createPlayer(playerId)
+{
+    let x =  getRandomInt(45, canvas.width - 45);
+    let y =  getRandomInt(45, canvas.height - 45);
+    players.push({
+        id:playerId,
+        x:x,
+        y:y,
+        color: "green"
+    });
 }
-
-function rightArrowPressed() {
-	var element = document.getElementById("image1");
-	element.style.left = parseInt(element.style.left) + 5 + 'px';
-}
-
-function upArrowPressed() {
-	var element = document.getElementById("image1");
-	element.style.top = parseInt(element.style.top) - 5 + 'px';
-}
-
-function downArrowPressed() {
-	var element = document.getElementById("image1");
-	element.style.top = parseInt(element.style.top) + 5 + 'px';
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function moveSelection(event) {
@@ -77,6 +105,36 @@ function moveSelection(event) {
 	}
 };
 
-function gameLoop() {
-	setTimeout("gameLoop()", 10);
+
+var lastTime = null;
+var cx;
+const canvas = document.querySelector("canvas");
+function frame(time) {
+    if(lastTime != null) {
+        update(Math.min(100, time - lastTime) / 1000);
+    }
+    lastTime = time;
+    requestAnimationFrame(frame);
 }
+
+function update(dt)
+{
+    cx.clearRect(0,0, canvas.width, canvas.height);
+    for(index = 0; index < players.length; ++index)
+    {
+        let player = players[index];
+
+        cx.beginPath();
+        cx.fillStyle = player.color;
+        cx.arc(player.x, player.y, 20, 0, Math.PI *2);
+        cx.fill();
+    }
+}
+
+window.onload = () => {
+    cx = canvas.getContext("2d");
+
+    requestAnimationFrame(frame);
+}
+
+
